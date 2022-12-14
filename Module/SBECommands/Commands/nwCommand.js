f7bb9b68d68fdd64bfccbab787e6aa1924a2335f
SaegusaMayumi1234 <69360824+SaegusaMayumi1234@users.noreply.github.com>
2022-12-14 14:15:59 +0700

    Pet color is now server side

--- Module/SBECommands/Commands/nwCommand.js
+++ Module/SBECommands/Commands/nwCommand.js
@@ -82,9 +82,9 @@ function nwDetails(data2, place) {
                 name += ` ${data2.data.types[place].items[i].name}`
             }
             name = fixUnicode(name) //.replace(/âœª/g, "&6✪").replace(/â�Ÿ/g, "&c✪").replace(/âšš/g, "⚚")
-            if (place === 'pets') {
-                name = `&7${name.replace("Common ", "&fCommon ").replace("Uncommon ", "&aUncommon ").replace("Rare ", "&9Rare ").replace("Epic ", "&5Epic ").replace("Legendary ", "&6Legendary ").replace("Mythic ", "&dMythic ").trim()}`;
-            }
+            // if (place === 'pets') {
+            //     name = `&7${name.replace("Common ", "&fCommon ").replace("Uncommon ", "&aUncommon ").replace("Rare ", "&9Rare ").replace("Epic ", "&5Epic ").replace("Legendary ", "&6Legendary ").replace("Mythic ", "&dMythic ").trim()}`;
+            // }
             hoverMsg.push(((data2.data.types[place].items[i]?.count || 1) > 1 ? "&7" + data2.data.types[place].items[i].count + "x&r " : "") + name + ` &b- ${addNotation("oneLetters", data2.data.types[place].items[i].price)}`)
         }
     }
